6f9bf7f1205388fcdae802650cee3d99f036625c
UAzif <begimot02@mail.ru>
2021-08-17 00:18:41 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 130;
+        int replenishmentAmount =2250;
+        float totalAmount;
+        float bonus =0;
+        if (replenishmentAmount > 1000)  {
+
+            bonus = replenishmentAmount/100;
+            totalAmount = initialAmount + replenishmentAmount + bonus;
+        }        else {
+                    totalAmount = initialAmount + replenishmentAmount;
+                }
+        System.out.println("Ваш счет составил: " + totalAmount);
+        System.out.println("В том числе бонус: " + bonus);
+    }
+}
