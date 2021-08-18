e63a6074991f05a874b91165266df29965375fd0
UAzif <begimot02@mail.ru>
2021-08-18 18:29:34 +0500

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int initialAmount = 130;
-        int replenishmentAmount =2250;
+        int replenishmentAmount = 2250;
         float totalAmount;
-        float bonus =0;
-        if (replenishmentAmount > 1000)  {
+        float bonus = 0;
+        if (replenishmentAmount > 1000) {
 
-            bonus = replenishmentAmount/100;
+            bonus = replenishmentAmount / 100;
             totalAmount = initialAmount + replenishmentAmount + bonus;
-        }        else {
-                    totalAmount = initialAmount + replenishmentAmount;
-                }
+        } else {
+            totalAmount = initialAmount + replenishmentAmount;
+        }
         System.out.println("Ваш счет составил: " + totalAmount);
         System.out.println("В том числе бонус: " + bonus);
     }
